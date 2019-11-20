2e7bf396ee101153a0b6926d9ba9fe8f8b26ac89
julian <juliohmhaha@hotmail.com>
2019-11-20 05:50:52 -0500

    asdasd

--- src/main/java/servlet/Medico.java
+++ src/main/java/servlet/Medico.java
@@ -54,7 +54,7 @@ public class Medico extends HttpServlet {
         } else if (url.equalsIgnoreCase("/historiaPaciente")) {
             
             try {
-            Date Fecha=new SimpleDateFormat("dd/MM/yyyy").parse(req.getParameter("fecha"));
+            Date Fecha=new SimpleDateFormat("yyyy/MM/dd").parse(req.getParameter("fecha"));
             String paciente = req.getParameter("paciente");
             String Especialista = req.getParameter("especialista");
             String Antecedentes_Familiares = req.getParameter("a_familiares");
